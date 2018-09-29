6a3c767e843cd358e16e0b3182e5b5a118b42a92
MontgomeryNugen <montgomery.nugen@gmail.com>
2018-09-29 10:38:10 -0400

    small changes

---
+++ src/main/java/com/prs/business/purchaserequest/PurchaseRequest.java
@@ -0,0 +1,118 @@
+package com.prs.business.purchaserequest;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class PurchaseRequest {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private int userID;
+	private String description;
+	private String justification;
+	private LocalDate dateNeeded;
+	private String deliveryMode;
+	private String status;
+	private double total;
+	private LocalDateTime submittedDate;
+
+	public PurchaseRequest(int id, int userID, String description, String justification, LocalDate dateNeeded,
+			String deliveryMode, String status, double total, LocalDateTime submittedDate) {
+		super();
+		this.id = id;
+		this.userID = userID;
+		this.description = description;
+		this.justification = justification;
+		this.dateNeeded = dateNeeded;
+		this.deliveryMode = deliveryMode;
+		this.status = status;
+		this.total = total;
+		this.submittedDate = submittedDate;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getJustification() {
+		return justification;
+	}
+
+	public void setJustification(String justification) {
+		this.justification = justification;
+	}
+
+	public LocalDate getDateNeeded() {
+		return dateNeeded;
+	}
+
+	public void setDateNeeded(LocalDate dateNeeded) {
+		this.dateNeeded = dateNeeded;
+	}
+
+	public String getDeliveryMode() {
+		return deliveryMode;
+	}
+
+	public void setDeliveryMode(String deliveryMode) {
+		this.deliveryMode = deliveryMode;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public LocalDateTime getSubmittedDate() {
+		return submittedDate;
+	}
+
+	public void setSubmittedDate(LocalDateTime submittedDate) {
+		this.submittedDate = submittedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "PurchaseRequest [id=" + id + ", userID=" + userID + ", description=" + description + ", justification="
+				+ justification + ", dateNeeded=" + dateNeeded + ", deliveryMode=" + deliveryMode + ", status=" + status
+				+ ", total=" + total + ", submittedDate=" + submittedDate + "]";
+	}
+
+}
